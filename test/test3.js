fbd7c10e2f42d00dd416127e16b99b38cb9ee6b4
Aparna Pansare <aparna.pansare@gmail.com>
2020-12-19 14:23:39 +0530

    test cases for ci cd

--- test/test3.js
+++ test/test3.js
@@ -1,6 +1,6 @@
 var request = require('supertest');
-//var app = require(‘../index.js’);
-var apps = require('../appi.js');
+var apps = require('../index.js');
+//var apps = require('../appi.js');
 
 describe('GET /', function() {
     it('test respond with hello world', function(done) { 
